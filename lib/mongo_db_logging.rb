3509c02e9d0a46c57b001c009ab721e4ce9a855f
Robert Sanders <robert@esquimaux.org>
2010-05-12 12:50:30 -0400

    more monitoring log excludes. also ignore if the url is /?monitor=1

--- lib/mongo_db_logging.rb
+++ lib/mongo_db_logging.rb
@@ -9,7 +9,9 @@ module MongoDBLogging
     return false if logged_in?
     user_agent = request.headers["USER_AGENT"]
     return true if user_agent.blank? ||
-            user_agent.match(/chartbeat|google|spider|relic|wormly|mon.?itor|service|crawl|index|bot|proxy/i)
+            user_agent.match(/chartbeat|google|spider|relic|wormly|mon.?itor|service|crawl|index|bot|proxy|basicstate/i)
+    return true if user_agent.match(/sucuri|scout|pingdom|nimbu|nagios/i)
+    return true if request.headers["PATH_INFO"] == "/" && ! request.params[:monitor].blank?
     return true if controller_name.match(/health|monitor/)
 
     false
