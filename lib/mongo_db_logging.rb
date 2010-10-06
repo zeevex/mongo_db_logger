40324553d96e6db1958802ac4d998d3247c1ae9b
Robert Sanders <robert@zeevex.com>
2010-10-06 14:29:12 -0400

    32707-440: mongo logger does not appear to be logging some requests - put in various conditions (including XHR and SSL) that will make mongo less likely to filter out requests. if there are other issues, like exceptions, this won't catch them.

--- lib/mongo_db_logging.rb
+++ lib/mongo_db_logging.rb
@@ -16,11 +16,15 @@ module MongoDBLogging
   end
 
   def mongo_ignore_request?
+    return false if request.ssl? || request.xhr?
     return false if logged_in?
     user_agent = request.headers["USER_AGENT"]
-    return true if !user_agent.blank? &&
-            user_agent.match(/chartbeat|google|spider|relic|wormly|mon.?itor|service|crawl|index|bot|proxy|basicstate/i)
-    return true if user_agent.match(/sucuri|scout|pingdom|nimbu|nagios/i)
+    return false if controller_name == "zesa"
+    if !user_agent.blank?
+      return true if
+            user_agent.match(/chartbeat|google|spider|relic|wormly|mon.?itor|service|crawl|pingdom|basicstate/i)
+      return true if user_agent.match(/sucuri|scout|pingdom|nimbu|nagios/i)
+    end
     return true if request.headers["PATH_INFO"] == "/" && ! request.params[:monitor].blank?
     return true if controller_name.match(/health|monitor/)
 
